7cd6914f632e64259a8c8d58d46c097a23fd7459
rish07 <rishirajsinghchhabra@gmail.com>
2021-02-27 21:52:09 +0530

    Around the world section

---
+++ lib/screens/around_world.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AroundTheWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text('AROUND THE WORLD'),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:huesofthemind_web/responsive_widget.dart';
+import 'package:huesofthemind_web/screens/around_world.dart';
 import 'package:huesofthemind_web/screens/coming_soon.dart';
 import 'package:huesofthemind_web/screens/home_page.dart';
 
@@ -8,8 +9,11 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        pageSnapping: false,
+        scrollDirection: Axis.vertical,
         children: [
           !ResponsiveWidget.isSmallScreen(context) ? HomePage() : ComingSoon(),
+          AroundTheWorld(),
         ],
       ),
     );
